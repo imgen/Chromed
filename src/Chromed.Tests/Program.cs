c5f455bff0f62d34b0b9257ba706a9b8bdc730bd
hailin <imgen@hotmail.com>
2021-10-13 14:26:33 +0800

    Initial setup

---
+++ src/Chromed.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Chromed;
+
+namespace Chromed.Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeZipper.ZipTo("E:/Chripe");
+        }
+    }
+}
---
+++ src/Chromed/ChromeZipper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.RuntimeInformation;
+
+namespace Chromed
+{
+    public static class ChromeZipper
+    {
+        public static void ZipTo(string targetDir, string zipBaseDir = null)
+        {
+            zipBaseDir ??= Environment.CurrentDirectory;
+            string zipFileRelativePath = GetZipRelativePath();
+            var zipFilePath = Path.Combine(zipBaseDir, zipFileRelativePath);
+            if (IsOSPlatform(OSPlatform.OSX))
+            {
+                NativeExtractToDirectory(zipFilePath, targetDir);
+            }
+            else
+            {
+                ZipFile.ExtractToDirectory(zipFilePath, targetDir);
+            }
+        }
+
+        private static void NativeExtractToDirectory(string zipPath, string folderPath)
+        {
+            using var process = new Process();
+            process.StartInfo.FileName = "unzip";
+            process.StartInfo.Arguments = $"\"{zipPath}\" -d \"{folderPath}\"";
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private static string GetZipRelativePath()
+        {
+            if (IsOSPlatform(OSPlatform.Windows))
+            {
+                return "runtimes/win-x64/native/chrome-win.zip";
+            }
+            else if (IsOSPlatform(OSPlatform.OSX))
+            {
+                return "runtimes/osx-x64/native/chrome-mac.zip";
+            }
+            else if (IsOSPlatform(OSPlatform.Linux))
+            {
+                return "runtimes/linux-x64/native/chrome-linux.zip";
+            }
+            else
+            {
+                throw new InvalidOperationException("Chromed only supports Windows, Mac and Linux");
+            }
+        }
+    }
+}
